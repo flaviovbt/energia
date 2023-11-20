b4cbbaf6c37c409c5c0f88c7051c4f16b0823980
flavio <flavio.vbt@yahoo.com>
2023-11-20 16:10:21 -0300

    links

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="footer">
-        <a href="#"><img src="../assets/unicamp.png" alt="unicamp"></a>
-        <a href="#"><img src="../assets/ft.png" alt="ft"></a>
-        <a href="#"><img class="ult" src="../assets/git.png" alt="git"></a>
+        <a href="https://www.unicamp.br/unicamp/"><img src="../assets/unicamp.png" alt="unicamp"></a>
+        <a href="https://www.ft.unicamp.br/"><img src="../assets/ft.png" alt="ft"></a>
+        <a href="https://github.com/flaviovbt?tab=repositories"><img class="ult" src="../assets/git.png" alt="git"></a>
     </div>
 </template>
 
