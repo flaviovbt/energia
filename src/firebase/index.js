cb97d8efcae3eb40ea52be071fc9c058282e7683
flavio <flavio.vbt@yahoo.com>
2023-11-20 16:24:34 -0300

    correcoes

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBJIZ7lnVE2itM8ykO_2RDw1_qyCd2gu4g",
-  authDomain: "energia-32371.firebaseapp.com",
-  projectId: "energia-32371",
-  storageBucket: "energia-32371.appspot.com",
-  messagingSenderId: "961002849858",
-  appId: "1:961002849858:web:770899257997b250fe4fdb"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
